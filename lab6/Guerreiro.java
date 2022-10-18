d0740f3e37c7492b04597d4b8bc567818fc89d4f
Iagopatrick <83458534+Iagopatrick@users.noreply.github.com>
2022-10-18 10:21:29 -0300

    mudança2

--- lab6/Guerreiro.java
+++ lab6/Guerreiro.java
@@ -1,12 +1,23 @@
 import java.util.Random;
 
-public class Guerreiro extends Combatente{
+public class Guerreiro extends Combatente implements Tanque{
 	private Arma espada = new Arma("sword", 25);
-    
+	resistencia = 50;
+
 	public Guerreiro(String identificacao){
     	super(identificacao);
 	}
     
+	public int resistencia(int damage){
+		resistencia -= damage;
+		if(resistencia < 0){
+			resistencia = 0
+		}
+		return resistencia;
+	}
+	
+
+
 	public int corte(){
     	return espada.cortar();
 	}
@@ -25,6 +36,14 @@ public class Guerreiro extends Combatente{
 			adversario.defender(corte());
 		}
 
+	}
+
+	@Override
+	public void defender(int ataque){
+		energia -= ataque - resistencia(ataque);
+		if (energia < 0){
+			energia = 0;
+		}
 	}
     
 }
---
+++ lab6/Tanque.java
@@ -0,0 +1,4 @@
+public abstract interface Tanque{
+    int resistencia;
+    int resistencia();
+}
